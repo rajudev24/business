ed6869e4760cbbf9a6cd6a075bb54caa02787f53
Raju Deb Nath <rajudevit94@gmail.com>
2023-11-12 11:48:24 +0600

    SEO added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,19 @@ import Choose from "@/components/Choose";
 import Feedback from "@/components/Feedback";
 import Header from "@/components/Header";
 import Service from "@/components/Service";
+import Head from "next/head"
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title> This is Home page </title>
+        <meta
+          name="description"
+          content="This is Landing page"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Header />
       <Banner />
       <Service />
